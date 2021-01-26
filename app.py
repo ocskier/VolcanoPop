974bc01a7bc2a63be5ba1de480d2251d1059f520
Jon Jackson <ocskier@gmail.com>
2021-01-26 13:59:37 -0500

    fix: not currently using sql db

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import os
 import mysql.connector
 import pandas
 
-connection = mysql.connector.connect(
-    user = "root",
-    password = os.getenv("SQL_PWD"),
-    host = 'localhost',
-    database = "db"
-)
+# connection = mysql.connector.connect(
+#     user = "root",
+#     password = os.getenv("SQL_PWD"),
+#     host = 'localhost',
+#     database = "db"
+# )
 
 volcano_data=pandas.read_csv('./Volcanoes.txt')
 marker_data=volcano_data.loc[0: len(volcano_data) ,'LAT':'LON']

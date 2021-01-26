e946858b3b32db1d87e50cfee335e123ecd0f3ef
Jon Jackson <ocskier@gmail.com>
2021-01-26 13:56:13 -0500

    feat: add read in volcano data with pandas

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import settings
 import os
 import mysql.connector
+import pandas
 
 connection = mysql.connector.connect(
     user = "root",
     password = os.getenv("SQL_PWD"),
     host = 'localhost',
-    database = "company_db"
+    database = "db"
 )
+
+volcano_data=pandas.read_csv('./Volcanoes.txt')
+marker_data=volcano_data.loc[0: len(volcano_data) ,'LAT':'LON']
+print(marker_data)

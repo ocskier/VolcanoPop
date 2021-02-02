402bb1427619926e40025d69226c261094f34561
Jon Jackson <ocskier@gmail.com>
2021-02-02 00:29:12 -0500

    feat: add multi-layer control

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ marker_data=volcano_data.loc[0: len(volcano_data) ,['LAT','LON','ELEV','NAME']]
 
 volcano_map = folium.Map(location=[marker_data.mean()['LAT'],marker_data.mean()['LON']],zoom_start=4,tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
+fg1 = folium.FeatureGroup(name="Volcanoes")
 
 def correctColor(value):
     if value < 2000:
@@ -42,11 +42,14 @@ for i in marker_data.index:
     """.format(height=el,name=name)
 
     iframe = folium.IFrame(html=html, width=200, height=100)
-    fg.add_child(folium.Marker(location=[lat,lon],popup=html,icon=folium.Icon(color=correctColor(int(el)),icon="fa-area-chart", prefix='fa')))
-    volcano_map.add_child(fg)
+    fg1.add_child(folium.Marker(location=[lat,lon],popup=html,icon=folium.Icon(color=correctColor(int(el)),icon="fa-area-chart", prefix='fa')))
+    volcano_map.add_child(fg1)
 
-fg.add_child(folium.GeoJson(data=(open('./world.json','r', encoding='utf-8-sig').read()),style_function=lambda x: {'fillOpacity':0.3,'fillColor': 'yellow' if x['properties']['POP2005'] <= 10000000 else 'darkgreen' if x['properties']['POP2005'] > 10000000 and x['properties']['POP2005'] <= 300000000 else 'blue'}))
+fg2 = folium.FeatureGroup(name='Population')
+fg2.add_child(folium.GeoJson(data=(open('./world.json','r', encoding='utf-8-sig').read()),style_function=lambda x: {'fillOpacity':0.3,'fillColor': 'yellow' if x['properties']['POP2005'] <= 10000000 else 'darkgreen' if x['properties']['POP2005'] > 10000000 and x['properties']['POP2005'] <= 300000000 else 'blue'}),)
 
+volcano_map.add_child(fg1)
+volcano_map.add_child(fg2)
 volcano_map.add_child(folium.LayerControl())
 
 volcano_map.save("./index.html")
